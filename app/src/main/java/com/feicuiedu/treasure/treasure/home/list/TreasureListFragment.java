15e8e45aba21acb2c5f7d26b61fa54a9d743a282
ccy1207 <1259621786@qq.com>
2016-10-08 11:42:25 +0800

    地图

---
+++ app/src/main/java/com/feicuiedu/treasure/treasure/home/list/TreasureListFragment.java
@@ -0,0 +1,20 @@
+package com.feicuiedu.treasure.treasure.home.list;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.feicuiedu.treasure.R;
+
+public class TreasureListFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_list,container,false);
+    }
+}
